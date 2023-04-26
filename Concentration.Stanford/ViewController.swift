5e8616338a300fc0bafc0883d9184cdf6c4a8021
gmekv <113336356+gmekv@users.noreply.github.com>
2023-04-26 21:15:51 +0400

    Initial Commit

---
+++ Concentration.Stanford/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration.Stanford
+//
+//  Created by Giorgi Meqvabishvili on 26.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
